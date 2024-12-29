529f01e809f20d734b267ef09a87b85156e4d9fd
Suryansh Anand <spirit@anandsur.com>
2024-12-29 16:18:46 +0530

    init

--- config/envs/development.js
+++ config/envs/development.js
@@ -1,3 +1,9 @@
 module.exports = {
-    
-}
+  CACHE_PREFIX: "cache-prefix",
+  CACHE_REDIS: "redis://localhost:6379",
+  CORTEX_PREFIX: "cortex-prefix",
+  CORTEX_REDIS: "redis://localhost:6379",
+  CORTEX_TYPE: "service-type",
+  OYSTER_REDIS: "redis://localhost:6379",
+  OYSTER_PREFIX: "oyster-prefix",
+};
